c61de9f35d5cacde7a20e58d3318ce426928d1bd
UryonBlade <matteo@nft-factory.club>
2025-04-09 18:44:21 +0200

    removes static files

--- public/storybook/assets/jsx-runtime-Cpff2dbc.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./jsx-runtime-BvH8MGSw.js";var s=r();export{s as j};
--- public/storybook/storybook/assets/preview-DMnweYT9.js
+++
@@ -1,1 +0,0 @@
-const a={parameters:{layout:"centered",controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},backgrounds:{default:"dark",values:[{name:"dark",value:"#000"}]}}};export{a as default};
