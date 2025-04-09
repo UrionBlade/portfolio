86c6ecbd6ea3e5c6a169b8aae31908a072311bd3
UryonBlade <matteo@nft-factory.club>
2025-04-09 10:56:42 +0200

    fix user scalability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <SpeedInsights />
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"
+        content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
       />
       <body className={poppins.className}>{children}</body>
     </html>
