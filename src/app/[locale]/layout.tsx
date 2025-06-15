8da64bb84de052079eebb31bb52bc35c1b5c2247
UryonBlade <matteo@nft-factory.club>
2025-06-15 18:48:34 +0200

    fix error on layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,46 +1,13 @@
-import type { Metadata, Viewport } from "next";
-import { Poppins } from "next/font/google";
-import "../globals.css";
-import "swiper/css";
-import "swiper/css/effect-creative";
-import "swiper/css/scrollbar";
-import "swiper/css/pagination";
 import { I18nProvider } from "@/i18n/I18nProvider";
-import { Toaster } from "react-hot-toast";
 
-const poppins = Poppins({
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-	subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-	title: "Matteo Poli",
-	description: "Costruisco esperienze digitali che fanno dire 'wow'!",
-};
-
-export const viewport: Viewport = {
-	initialScale: 1,
-	maximumScale: 1,
-	minimumScale: 1,
-	width: "device-width",
-	userScalable: false,
-};
-export default async function RootLayout({
+export default async function Layout({
 	children,
 	params,
 }: {
 	children: React.ReactNode;
-	params: { locale: string };
+	params: Promise<{ locale: string }>;
 }) {
-	return (
-		<html lang={params.locale}>
-			<body className={`${poppins.className} antialiased`}>
-				<I18nProvider locale={params.locale}>{children}</I18nProvider>
-				<Toaster
-					position="top-center"
-					toastOptions={{ duration: 3000, style: { marginTop: "2rem" } }}
-				/>
-			</body>
-		</html>
-	);
+	const { locale } = await params;
+
+	return <I18nProvider locale={locale}>{children}</I18nProvider>;
 }
---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata, Viewport } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import "swiper/css";
+import "swiper/css/effect-creative";
+import "swiper/css/scrollbar";
+import "swiper/css/pagination";
+import { Toaster } from "react-hot-toast";
+
+const poppins = Poppins({
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Matteo Poli",
+	description: "Costruisco esperienze digitali che fanno dire 'wow'!",
+};
+
+export const viewport: Viewport = {
+	initialScale: 1,
+	maximumScale: 1,
+	minimumScale: 1,
+	width: "device-width",
+	userScalable: false,
+};
+
+export default async function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="it">
+			<body className={`${poppins.className} antialiased`}>
+				{children}
+				<Toaster
+					position="top-center"
+					toastOptions={{ duration: 3000, style: { marginTop: "2rem" } }}
+				/>
+			</body>
+		</html>
+	);
+}
